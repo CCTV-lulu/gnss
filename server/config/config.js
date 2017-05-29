ae7e2f14676d22b21ca67b0efe821f573a2a9a2c
chenyuey <wodechangemail@gmail>
2017-05-29 18:54:21 +0800

    set host

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     production: {
         rootPath: rootPath,
-        db: 'mongodb://root:123@localhost:27017/admin?authSource=admin',
+        db: 'mongodb://root:123@localhost:27017/wang?authSource=admin',
         port: process.env.PORT || 30000,
         queueName: 'raw_prod_1',
         logPath:'/logs/',
