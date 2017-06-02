a5d2845797bde652ca448fdf371f28b5445f4ef6
chenyuey <wodechangemail@gmail>
2017-06-02 14:34:25 +0800

    删除注册

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,6 @@
 var auth = require('./auth'),
     controllers = require('../controllers');
 module.exports = function(app) {
-    app.get('/register', controllers.users.getRegister);
-    app.post('/register', controllers.users.createUser);
-
     app.post('/login', auth.login);
     app.get('/logout', auth.logout);
     app.get('/checkLogin',auth.isAuthenticated);
@@ -13,37 +10,53 @@ module.exports = function(app) {
     });
 
     //todo check login
-    app.get('/getStationStatus', auth.isInRole('user'),controllers.station.getStationStatus);
-    app.post('/userStaId', auth.isInRole('user') ,controllers.station.updateStaId);
 
+
+
+
+
+
+
+//    app.post('/staThreshold', controllers.station.saveThreshold);
+
+    //todo check admin
+    app.post('/changePassword',controllers.users.changePassword);
+    app.get('/findAllUsers',  auth.isInRole('admin'), controllers.users.getAllUser);
+
+    app.post('/addAdmin',controllers.users.addAdmin);
+    app.post('/addUser',controllers.users.addUser);
+
+
+    app.get('/downloadStaData',  auth.isInRole('user'), controllers.station.downloadStaData);
+
+
+    app.post('/userStaId', auth.isInRole('user') ,controllers.station.updateStaId);
     app.post('/userStartStaId', auth.isInRole('user') ,controllers.station.updateStaId);
 
-    app.get('/userStaId', auth.isInRole('user'),controllers.station.getUserStationId);
 
+    app.get('/userStaId', auth.isInRole('user'),controllers.station.getUserStationId);
+    app.get('/getStationStatus', auth.isInRole('user'),controllers.station.getStationStatus);
+
+    app.post('/StationConfig', controllers.users.getStationConfig);
+
+
+
+
+    app.get('/getStation', auth.isInRole('user'), controllers.station.getStations);
+    app.post('/findSatData', auth.isInRole('user'), controllers.station.setUserFindTime);
+    app.post('/getStaThreshold',  auth.isInRole('user'), controllers.station.getUserStaThreshold);
+    app.post('/setStaThreshold',  auth.isInRole('user'), controllers.station.setStaThreshold);
+    app.get('/getUserFindStaData', auth.isInRole('user'), controllers.station.getUserFindStaData);
+
+
+    app.get('/addStation', auth.isInRole('admin'),controllers.station.createStation);
+
+    app.post('/deleteUser', auth.isInRole('admin'),controllers.users.deleteUser);
+    app.post('/deleteStation', auth.isInRole('admin'),controllers.station.deleteStation);
+    app.post('/addStation', auth.isInRole('admin'),controllers.station.addStation)
 
     app.get('/getStartInfo',function (req,res) {
         res.send(true)
     });
-
-    app.post('/getStaThreshold',  auth.isInRole('user'), controllers.station.getUserStaThreshold);
-    app.post('/setStaThreshold',  auth.isInRole('user'), controllers.station.setStaThreshold);
-    app.get('/getStation', auth.isInRole('user'), controllers.station.getStations);
-    app.post('/findSatData', auth.isInRole('user'), controllers.station.setUserFindTime);
-    app.get('/getUserFindStaData', auth.isInRole('user'), controllers.station.getUserFindStaData);
-    app.get('/downloadStaData',  auth.isInRole('user'), controllers.station.downloadStaData);
-//    app.post('/staThreshold', controllers.station.saveThreshold);
-
-    //todo check admin
-    app.post('/StationConfig', controllers.users.getStationConfig);
-    app.post('/changePassword',controllers.users.changePassword);
-    app.get('/findAllUsers',  auth.isInRole('admin'), controllers.users.getAllUser);
-
-    app.get('/addStation', auth.isInRole('admin'),controllers.station.createStation);
-
-        app.post('/addAdmin',controllers.users.addAdmin);
-    app.post('/addUser',controllers.users.addUser);
-    app.post('/deleteUser', auth.isInRole('admin'),controllers.users.deleteUser);
-    app.post('/deleteStation', auth.isInRole('admin'),controllers.station.deleteStation);
-    app.post('/addStation', auth.isInRole('admin'),controllers.station.addStation)
 
 };
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,39 +2,7 @@ var encryption = require('../utilities/cripto'),
     User = require('../data/usersData');
     fs = require("fs");
 module.exports = {
-    getRegister: function (req, res, next) {
-        if (req.user) {
-            res.redirect('/');
-        } else {
-            res.redirect('users/register');
-        }
-    },
-    createUser: function (req, res, next) {
-        var newUserData = req.body;
-        if (newUserData.password !== newUserData.confirmPassword) {
-            req.session.error = 'Passwords do not match!';
-            res.redirect('/register');
-        } else {
-            newUserData.salt = encryption.generateSalt();
-            newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
-            User.createUser(newUserData, function (err, user) {
-                if (err) {
-                    req.session.error = 'Username exists!';
-                    res.redirect('/register');
-                    return;
-                }
-                req.logIn(user, function (err) {
-                    if (err) {
-                        res.status(400);
-                        return res.send({reason: err.toString()});
-                    }
-                    else {
-                        res.redirect('/');
-                    }
-                });
-            });
-        }
-    },
+
     updateUser: function (req, res, next) {
         if (req.user._id == req.body._id || req.user.roles.indexOf('admin') > -1) {
             var updatedUserData = req.body;
