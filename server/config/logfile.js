791c007a105b60cd3402bb57b49564a7825a222c
chenyuey <wodechangemail@gmail>
2017-06-15 09:38:41 +0800

    remove bug

--- server/config/logfile.js
+++ server/config/logfile.js
@@ -16,7 +16,7 @@ var os=require('os');
 
 module.exports = function (app) {
     app.post('/logs', upload.single('log_file'), function (req, res, next) {
-        console.lg('/--------------get log')
+        console.log('/--------------get log')
         var logPath = config.logPath.toString() + req.file.originalname;
         var logResolvePath = cwd+logPath;
         var name = fs.rename(req.file.path, logResolvePath);
