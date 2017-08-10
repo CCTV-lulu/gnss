4e5d1a60568ef0eb89bc663377093c54adacfc3a
CCTV-lulu <1253139642@qq.com>
2017-08-10 16:24:01 +0800

    修改接受文件的方法

--- server/config/logfile.js
+++ server/config/logfile.js
@@ -26,53 +26,53 @@ logProcess.init()
 
 module.exports = function (app) {
 
-    app.post('/logs',function (req, res, next) {
-            //file_op.rm('uploads');
-            var file_op    = require('../util/file_operate');
-            var formidable = require('formidable');
-            // file_op.mkdirsSync( './../uploads/');
-            var form = new formidable.IncomingForm();
-            var name
-            form.maxFieldsSize = 100 * 1024 * 1024;
-            form.maxFields = 0;
-            form.uploadDir = '../uploads'
-            form.parse(req, function(err, fields, files) {
-                if(!err){
-                     name = fs.rename(files.log_file.path, cwd+'/logs/'+files.log_file.name, function () {
-                        logProcess.addLogPath('/logs/' + files.log_file.name, function (result) {
-                            if (result.status) {
-                                return res.send('ok')
-                            }
-                            res.status(404)
-                                .send('Not Found')
-                        })
-                    })
-
-                }else{
-                    console.log('---------------err')
-                    console.log(err)
-                    res.status(404)
-                             .send('Not Found')
-                }
-
-            });
-
-
-
-    });
-    // app.post('/logs', upload.single('log_file'), function (req, res, next) {
-    //     var logPath = config.logPath.toString() + req.file.originalname;
-    //     var logResolvePath = cwd + logPath;
-    //     var name = fs.rename(req.file.path, logResolvePath, function () {
-    //         logProcess.addLogPath(logPath, function (result) {
-    //             if (result.status) {
-    //                 return res.send('ok')
+    // app.post('/logs',function (req, res, next) {
+    //         //file_op.rm('uploads');
+    //         var file_op    = require('../util/file_operate');
+    //         var formidable = require('formidable');
+    //         // file_op.mkdirsSync( './../uploads/');
+    //         var form = new formidable.IncomingForm();
+    //         var name
+    //         form.maxFieldsSize = 100 * 1024 * 1024;
+    //         form.maxFields = 0;
+    //         form.uploadDir = '../uploads'
+    //         form.parse(req, function(err, fields, files) {
+    //             if(!err){
+    //                  name = fs.rename(files.log_file.path, cwd+'/logs/'+files.log_file.name, function () {
+    //                     logProcess.addLogPath('/logs/' + files.log_file.name, function (result) {
+    //                         if (result.status) {
+    //                             return res.send('ok')
+    //                         }
+    //                         res.status(404)
+    //                             .send('Not Found')
+    //                     })
+    //                 })
+    //
+    //             }else{
+    //                 console.log('---------------err')
+    //                 console.log(err)
+    //                 res.status(404)
+    //                          .send('Not Found')
     //             }
-    //             res.status(404)
-    //                 .send('Not Found')
-    //         })
-    //     })
-    // })
+    //
+    //         });
+    //
+    //
+    //
+    // });
+    app.post('/logs', upload.single('log_file'), function (req, res, next) {
+        var logPath = config.logPath.toString() + req.file.originalname;
+        var logResolvePath = cwd + logPath;
+        var name = fs.rename(req.file.path, logResolvePath, function () {
+            logProcess.addLogPath(logPath, function (result) {
+                if (result.status) {
+                    return res.send('ok')
+                }
+                res.status(404)
+                    .send('Not Found')
+            })
+        })
+    })
 
 };
 
