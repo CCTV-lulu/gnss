8cfebe21aafabaf176f23e1d2af958d91d8ff8e0
CCTV-lulu <1253139642@qq.com>
2017-08-10 16:45:53 +0800

    不接受实时数据

--- server/canavprocess/realtime_process.js
+++ server/canavprocess/realtime_process.js
@@ -241,5 +241,5 @@ function real_pos(para,obs,nav,prcopt,sol,sys,logjson) {
     nodepos.posShowStruct(para, sys, logjson);
     logjson.posR[sys].trackNum = obs.length;
     //nodepos.eleUpdate(para);
-    console.log(sys,sol.time,cmn.time2string_Local(sol.time), sol.pos, logjson.posR[sys].HPL,logjson.posR[sys].VPL,logjson.posR[sys].posNum,logjson.posR[sys].trackNum);
+    // console.log(sys,sol.time,cmn.time2string_Local(sol.time), sol.pos, logjson.posR[sys].HPL,logjson.posR[sys].VPL,logjson.posR[sys].posNum,logjson.posR[sys].trackNum);
 }
