4d9cccd917fbe9aeb944bf8f0f6592f197dd0d6a
CCTV-lulu <1253139642@qq.com>
2017-08-18 14:33:52 +0800

    修改代码尝试部署

--- models.js
+++ models.js
@@ -4,7 +4,7 @@ function install() {
     child_process.exec('rm node_modules -r',{maxBuffer: 1024 * 500000000},function (err,stdout,stderr) {
         // rmdir /s/q node_modules
 
-        child_process.exec('npm install -f',{maxBuffer: 1024 * 500000000},function (err,stdout,stderr) {
+        child_process.exec('printf "yes\n\n\n\n" | npm install ',{maxBuffer: 1024 * 500000000},function (err,stdout,stderr) {
             if(err) {
                 console.log("安装失败，正在尝试重新安装...")
                 install()
